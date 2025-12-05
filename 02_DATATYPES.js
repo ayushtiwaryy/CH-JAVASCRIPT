1a3e3291bd2b8ac56cbd10b133629263bb1c22e9
ayushtiwaryy <ayushtiwary353@gmail.com>
2025-12-05 14:21:55 +0530

    first commit

---
+++ 02_DATATYPES.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all JS code as newer version
+
+// ************DATA TYPES********
+
+let name = "hitesh" // string
+let age = 23 // integer
+let isLoggedIN = false // boolean
+//null : standalone value/representation of empty value
+//undefined : anything not defined
+//symbol 
+//object
